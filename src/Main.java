9c12ea1a843247ab04ab8b93667e87e5021a34b7
Andrew Ball <andy.bob.ball@gmail.com>
2020-04-04 00:12:55 -0500

    some changes

--- src/Main.java
+++ src/Main.java
@@ -32,22 +32,30 @@ public class Main {
 
         boolean userConfirm = true;
         do {
-            System.out.println("Would you like to find a number or enter a new number? Find/New");
+            System.out.println("Would you like to: \nFind a number: FIND\nEnter a new number: NEW\nOr delete a number: DELETE\nPlease Enter, Find: New: Delete");
             String confirm = sc.next().toUpperCase();
-            System.out.println(confirm);
+//            System.out.println(confirm);
             if (confirm.contains("NEW")) {
                 String line = getContact();
-                String topLine = "Name | Phone Number \n -----------------------";
+                String topLine = "\t\tName | Phone Number \n -----------------------";
                 Files.write(contactFilePath, Arrays.asList(topLine), StandardOpenOption.CREATE);
                 Files.write(contactFilePath, Arrays.asList(line), StandardOpenOption.APPEND);
+            }if (confirm.contains("FIND")) {
+
+                // readList needs to only print name or not exist at all...
+                List<String> readList = Files.readAllLines(contactFilePath);
+
+                //for loop iterates and prints all contacts
+                for (var i = 0; i < readList.size();i++) {
+                    System.out.println("\n" + readList.get(i));
+                }
+                System.out.println("Please Enter a Name From The List: ");
+                String nameFromList = sc.next().toUpperCase();
+                // nameFromList needs to iterate through the contacts and return contact info.
             }
-//            if (confirm.contains("Find")) {
-//
-//            }
-            //begin terminal interactive
+//            begin terminal interactive
 
             System.out.println("Do you wish to continue? [y/n]");
-
             String userResponse = sc.next().toLowerCase();
             if (!userResponse.equalsIgnoreCase("y")) {
                 userConfirm = false;
@@ -56,15 +64,17 @@ public class Main {
     }
 
     public static String getContact() {
-        System.out.print("Enter A Name: ");
-        String contactName = sc.next();
+        System.out.print("Enter First Name: ");
+        String contactFirstName = sc.next();
+        System.out.print("Enter Last Name: ");
+        String contactLastName = sc.next();
         System.out.print("Enter A Phone Number: ");
         String contactNumber = sc.next();
-        String[] splitNumber;
-        splitNumber = contactNumber.split("");
+//        String[] splitNumber;
+//        splitNumber = contactNumber.split("");
         contactNumber.contains("0123456789");
-        System.out.println(splitNumber);
-        return contactName + " | " + contactNumber + "\n";
+//        System.out.println(splitNumber);
+        return contactFirstName +" " + contactLastName + " | " + contactNumber ;
     }
 
 
